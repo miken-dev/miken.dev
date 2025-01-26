51883827f022f9c2bd5b96588b6867101587dcff
Michael Neal <mike@miken.dev>
2025-01-26 02:21:33 -0500

    configure mikendev theme

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,12 +3,27 @@ import forms from '@tailwindcss/forms';
 import typography from '@tailwindcss/typography';
 import type { Config } from 'tailwindcss';
 
+import { skeleton, contentPath } from '@skeletonlabs/skeleton/plugin'
+import * as themes from '@skeletonlabs/skeleton/themes'
+import mikendevtheme from './mikendevtheme.ts'
+
 export default {
-	content: ['./src/**/*.{html,js,svelte,ts}'],
-
+	
+	content: [
+		'./src/**/*.{html,js,svelte,ts}',
+		contentPath(import.meta.url, 'svelte')
+	],
 	theme: {
-		extend: {}
+		[
+		themes.mikendevtheme
+		]	
 	},
-
-	plugins: [typography, forms, containerQueries]
+	plugins: [
+		skeleton({
+			// NOTE: each theme included will increase the size of your CSS bundle
+			themes: [ themes.cerberus, themes.rose ]
+		})
+	]
 } satisfies Config;
+
+

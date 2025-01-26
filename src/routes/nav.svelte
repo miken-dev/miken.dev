90095c4f9064ed93e71b75fb2778dc5665385eae
Michael Neal <mike@miken.dev>
2025-01-26 15:23:52 -0500

    create nav

---
+++ src/routes/nav.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	import { Navigation } from "@skeletonlabs/skeleton-svelte";}
+	import IconMenu from 'lucide-svelte/icons/menu'
+</script>
+
+<header>
+	<div class="logo">
+		<img src="https://placehold.co/300x100" />
+	</div>
+	<div class="links"
+</header>

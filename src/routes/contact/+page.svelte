0198f61b69c01bbc1fda99a5710a23374f81b76c
Michael Neal <mike@miken.dev>
2025-01-27 02:26:53 -0500

    create contact form

--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -1,6 +1,20 @@
 <script lang="ts">
+	import { Input, Label, Textarea, Button } from 'flowbite-svelte';
 </script>
 
-<main>
-	
+<main class="dark:bg-slate-800 py-60 px-96 ">
+	<h1 class="text-xl dark:text-slate-50">Reach out!</h1>
+	<p class="dark:text-slate-50 pb-8">If you're interested in hiring me as a consultant, or just have some feedback on one of my apps, feel free to reach out below!</p>
+	<form name="contact" method="POST" data-netlify="true">
+		<Label for="name" class="block mb-2">Name</Label>
+		<Input id="name" />
+
+		<Label for="companyname" class="block mb-2 pt-6">Company Name</Label>
+		<Input id="companyname" />
+		<Label for="emailaddress" class="block mb-2 pt-6">Email Address</Label>
+		<Input id="emailaddress" />
+		<Label for="message" class="block mb-2 pt-6">Your message</Label>
+		<Textarea id="message" />
+		<Button type="submit" form="contact" value="Submit" class="mt-6">Submit</Button> 
+	</form>
 </main>

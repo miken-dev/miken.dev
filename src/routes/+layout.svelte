99a567fd9caeaf0549983872d20dd5a66e33cb6f
Michael Neal <mike@miken.dev>
2025-01-26 22:15:35 -0500

    update header with logo and social links

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,35 +1,37 @@
 <script lang="ts">
 	import '../app.css';
 	import { page } from '$app/state';
+	import Logo from '$lib/Logo.svg'
+	import Footer from '$lib/Footer.svelte';
 
 	import { Navbar, NavBrand, NavLi, NavUl, NavHamburger } from 'flowbite-svelte';
 	import { DarkMode } from 'flowbite-svelte';
 
-	import { Github } from 'lucide-svelte';	
-	import { Twitter } from 'lucide-svelte'
+	import { GithubSolid, TwitterSolid } from 'flowbite-svelte-icons';	
 	let { children } = $props();
 
 	let activeURL = $derived(page.url.pathname);
 </script>
 
-<Navbar>
+<Navbar class="dark:bg-slate-900">
 	<NavBrand href="/">
-		<img src="https://placehold.co/200x100" class="me-3 h-6 sm:h-9" alt="miken.dev logo" />
+		<img src="{Logo}" class="me-3 h-6 sm:h-9" alt="miken.dev logo" />
 		<span class="self-center whitespace-nowrap text-xl font-semibold dark:text-white"
 			>miken.dev</span
 		>
 	</NavBrand>
 	<NavHamburger />
-	<NavUl {activeURL}>
-		<NavLi href="/">Home</NavLi>
+	<NavUl {activeURL} class="">
 		<NavLi href="/freelance">Freelance</NavLi>
 		<NavLi href="/mobileapps">Mobile Apps</NavLi>
 		<NavLi href="/about">About</NavLi>
 		<NavLi href="/contact">Contact Me</NavLi>
-		<NavLi href="https://github.com/miken-dev"><Github /></NavLi>
-		<NavLi href="https://bsky.app/profile/miken.dev"><Twitter /> </NavLi>
-		<NavLi><DarkMode /></NavLi>
+		<NavLi href="https://github.com/miken-dev"><GithubSolid /></NavLi>
+		<NavLi href="https://bsky.app/profile/miken.dev"><TwitterSolid /> </NavLi>
+		<NavLi><DarkMode class="p-0"/></NavLi>
 	</NavUl>
 </Navbar>
 
 {@render children()}
+
+<Footer />
